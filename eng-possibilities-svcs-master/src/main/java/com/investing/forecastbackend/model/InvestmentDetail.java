c54e3818078a7b3f2e0e9ad3bdbe0b1c84e9254f
Isha Palakurthy <ipalakurthy3@gatech.edu>
2021-02-23 21:01:44 -0500

    initial backend changes

--- eng-possibilities-svcs-master/src/main/java/com/investing/forecastbackend/model/InvestmentDetail.java
+++ eng-possibilities-svcs-master/src/main/java/com/investing/forecastbackend/model/InvestmentDetail.java
@@ -1,5 +1,16 @@
 package com.investing.forecastbackend.model;
+import java.util.*;
+
 
 // TODO Model the data read from ../resources/data/investment-details.json
 public class InvestmentDetail {
+    String category;
+    String minimum;
+    String[] data;
+
+    public InvestmentDetail(String cate, String min, String[] data) {
+        this.category = cate;
+        this.minimum = min;
+        this.data = data;
+    }
 }
--- eng-possibilities-svcs-master/src/main/java/com/investing/forecastbackend/service/InvestingForecastService.java
+++ eng-possibilities-svcs-master/src/main/java/com/investing/forecastbackend/service/InvestingForecastService.java
@@ -1,15 +1,22 @@
 package com.investing.forecastbackend.service;
 
 import com.investing.forecastbackend.model.ForecastRequest;
+
+import org.json.simple.*;
+import org.json.simple.parser.*;
+
+
+import java.util.Iterator;
+
 import com.investing.forecastbackend.model.ForecastResponse;
 import com.investing.forecastbackend.model.InvestmentDetail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
-import static java.util.Collections.emptyList;
+import java.util.*;
+import java.io.FileReader;
 
 @Service
 @Slf4j
@@ -18,12 +25,46 @@ public class InvestingForecastService {
 
     public List<InvestmentDetail> getInvestmentOptions() {
         // TODO read investment options from investment-details.json
-        return emptyList();
+        JSONParser parser = new JSONParser();
+
+        //Object jObj = parser.parse(new FileReader("/Users/sheonmwapinza/OneDrive - PennO365/JAVAWORKSPACE/GS–Project/eng-possibilities-svcs-master/src/main/resources/data/investment-details.json"));
+        List<InvestmentDetail> listInvestmentDetail = new ArrayList<>();
+        try {
+            //file directory starts with src
+            Object investmentObject = parser.parse(new FileReader("/src/main/resources/data/investment-details.json"));
+            JSONObject jsonObject = (JSONObject)investmentObject;
+            JSONArray investments = (JSONArray)jsonObject.get("Investments");
+
+            //Iterator iterator = investments.iterator();
+            for(int j = 0; j<investments.size(); j++) {
+
+                JSONObject investment = (JSONObject) investments.get(j);
+
+                JSONArray dataArray = (JSONArray)investment.get("data");
+                String[] data = new String[dataArray.size()];
+
+                for(int i = 0; i < dataArray.size(); i++) {
+                    data[i] = (String) dataArray.get(i);
+                }
+
+                InvestmentDetail investDetail = new InvestmentDetail((String)investment.get("category"), (String)investment.get("minimum"),
+                        data);
+
+                listInvestmentDetail.add(investDetail);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.print(listInvestmentDetail.toString());
+        return listInvestmentDetail;
     }
 
     public ForecastResponse getInvestmentOptions(final ForecastRequest request) {
         // TODO write algorithm to calculate investment forecast from request configuration
+        //get the request form
         return new ForecastResponse();
     }
 
 }
+
